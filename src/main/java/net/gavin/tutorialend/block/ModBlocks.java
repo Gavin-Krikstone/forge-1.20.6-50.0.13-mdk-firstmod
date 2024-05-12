91c76dd5e66e78b135075c7b60f9cada7d715f55
gavin <gavin@deinon.com>
2024-05-11 23:02:39 -0500

    10:25 SapphireBlock Added

--- src/main/java/net/gavin/tutorialend/block/ModBlocks.java
+++ src/main/java/net/gavin/tutorialend/block/ModBlocks.java
@@ -1,6 +1,8 @@
 package net.gavin.tutorialend.block;
 
 import net.gavin.tutorialend.TutorialEnd;
+import net.gavin.tutorialend.item.ModItems;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -10,25 +12,27 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialEnd.MOD_ID);
 
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
 
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BlOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
-        return toReturn
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItems(name, toReturn);
+        return toReturn;
     }
 
 
     private static <T extends Block>RegistryObject<Item> registerBlockItems(String name, RegistryObject<T> block) {
-        return ModItems.Items.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
     public static void register(IEventBus eventBus) {
